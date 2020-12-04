cafe724c9ac7fdd8e8ec52c832d4644c7067a3b6
Бруйко Евгения <bruyko.e@gmail.com>
2020-12-04 23:36:40 +0300

    Initial Commit

---
+++ Shoping List for my Frends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shoping List for my Frends
+//
+//  Created by Brubrusha on 12/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
